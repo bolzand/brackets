3e8d01a2ecd9e0ffa2b706a0b99644661c24c973
Dmitry Bolzan <dmitry.bolzan@gmail.com>
2019-10-09 23:30:58 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = {};
+  for (let i = 0; i < bracketsConfig.length; i++){
+    let element = bracketsConfig[i];
+    config[element[1]] = element[0];
+  }
+
+  let stack = [];
+  for (let i = 0; i< str.length; i++){
+    let symbol = str[i];
+    if (stack.length && config[symbol] == stack[stack.length - 1]){
+      stack.pop();
+    } else {
+      stack.push(symbol);
+    }
+  }
+
+  return stack.length == 0;
 }
